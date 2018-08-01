e7824e224b9b136dd3718cf76443ec030abbff7d
Joe Jackson <ph8tel@gmail.com>
2018-08-01 15:35:06 -0700

    remote underscore set

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,8 +7,8 @@ const key = process.env.KEY
 const app = {
   init: () => {
     $.ajaxPrefilter(function(settings, _, jqXHR) {
-      jqXHR.setRequestHeader('X-Parse-Application-Id', id);
-      jqXHR.setRequestHeader('X-Parse-REST-API-Key', key);
+      jqXHR.setRequestHeader('X-Parse-Application-Id', '28D19FA3745BA5570D04B4C37461926637EB45FF');
+      jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'D89B0AB80513AA4936590C82C75F81E80C2BF4E3');
    });
     $(document).ready(() => {
       $('#newlobby').hide();
